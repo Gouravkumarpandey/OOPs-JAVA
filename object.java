026bafd32751ac48396c439cb532f084bbb34b1a
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-09-17 14:18:10 +0530

    add

--- object.java
+++ object.java
@@ -1,6 +1,23 @@
 class Student {
-    string name;
-    string age;
+    String name;
+    int age;
 
-    public  void  print
+    public  void  printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    Student( ){
+        System.out.println("Constructor Called");
+
+    }
+}
+class Object{
+    public static void main(String arg[]){
+        Student s1 = new Student();
+        s1.name = "Rohit";
+        s1.age = 22;
+
+        s1.printInfo();
+    }
 }
