b0c8af8c84858d426e1c43e968fd38440b4ee6b2
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-09-19 21:16:44 +0530

    add

--- grpanagram.cpp
+++ grpanagram.cpp
@@ -7,7 +7,20 @@ using namespace std;
 
 class Solution {
 
-    vector<
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> frequencyMap;
+
+        for (string& str : strs) {
+            string key = getFrequencyString(str);  // build frequency signature
+            frequencyMap[key].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : frequencyMap) {
+            result.push_back(entry.second);
+        }
+        return result;
+    }
 }
 
 
